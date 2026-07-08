07645b6da45362130f41cb9c66db8a4c69e5571a
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-07-08 20:12:56 +0600

    git to

--- default-para.js
+++ default-para.js
@@ -1,8 +1,17 @@
-function add(num1, num2){
+// function add(num1, num2){
+//     const total = num1 + num2;
+//     console.log(num1, num2, total);
+
+// }
+
+
+// add(10);
+
+
+function add2(num1, num2 = 0){
     const total = num1 + num2;
     console.log(num1, num2, total);
 
 }
 
-
-add(10);
+add2(10, 60);
