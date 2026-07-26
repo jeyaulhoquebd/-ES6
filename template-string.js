7daff14c6d8d628cf050231cdc99c1885c6f5802
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-07-26 11:43:23 +0600

    es6 git

--- template-string.js
+++ template-string.js
@@ -8,3 +8,21 @@ const mony = `gib me mony ${10 + 20 - 85 + 30 *58 +20 / 2}`;
 
 console.log(AboutMe);
 console.log(mony);
+
+
+function getcarthtml(name , description , price ){
+    const cart =`
+    <div class="cart">
+     <h1> my cart Nmme is ${name}</h1>
+     <h2> My cart description is ${description}</h2>
+     <p> My cart price is ${10 + 20 - 85 + 30 *58 +20 / 2}</p>
+
+
+    </div>
+
+    `;
+
+    console.log(cart);
+}
+
+getcarthtml('Iphone 14 Pro Max' , 'This is my new phone' , 1200);
