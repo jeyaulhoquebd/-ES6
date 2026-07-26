3405b1544083401963837eb8457e51aa3d7aafa0
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-07-26 11:31:14 +0600

    es6 git

--- template-string.js
+++ template-string.js
@@ -2,4 +2,9 @@ const fastName = 'Jeyaul'
 const lastName ='Hoqeu'
 
 const AboutMe  = `My Name is ${fastName}`;
+
+
+const mony = `gib me mony ${10 + 20 - 85 + 30 *58 +20 / 2}`;
+
 console.log(AboutMe);
+console.log(mony);
